74803eee15a795ac5e9b13e6047f301ec48118e6
Rafael Goretti De Deus <rafael.deus@idor.org>
2025-05-01 18:51:15 -0300

    Ajustando XUnit para mudanca de nomenclatura de metodos e atributos

---
+++ FluxoCaixaServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using FluxoCaixa.Models;
+using FluxoCaixa.Repositories;
+using FluxoCaixa.Services;
+using System;
+using Xunit;
+
+namespace FluxoCaixa.Tests
+{
+    public class FluxoCaixaServiceTests
+    {
+        private readonly LancamentoRepository _repository;
+        private readonly FluxoCaixaService _service;
+
+        public FluxoCaixaServiceTests()
+        {
+            _repository = new LancamentoRepository();
+            _service = new FluxoCaixaService(_repository);
+        }
+
+        [Fact]
+        public void Acusa_Lancamento_Passado()
+        {
+            var lancamento = new Lancamento
+            {
+                Data = DateTime.Today.AddDays(-1),
+                TipoDeLancamento = TipoLancamento.Recebimento,
+                Descricao = "Teste",
+                Conta = "12345-6",
+                Banco = "Banco X",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "123.456.789-00",
+                Valor = 100
+            };
+
+            var podeLancar = _service.ValidarLancamento(lancamento, out string motivo);
+
+            Assert.False(podeLancar);
+            Assert.Equal("Não é permitido lançar com data no passado.", motivo);
+        }
+
+        [Fact]
+        public void Acusa_Saldo_Negativo_Limite_Ultrapassado()
+        {
+            var lancamento = new Lancamento
+            {
+                Data = DateTime.Today,
+                TipoDeLancamento = TipoLancamento.Pagamento,
+                Descricao = "Pagamento Grande",
+                Conta = "12345-6",
+                Banco = "Banco Y",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "123.456.789-00",
+                Valor = 25000m
+            };
+
+            var podeLancar = _service.ValidarLancamento(lancamento, out string motivo);
+
+            Assert.False(podeLancar);
+            Assert.Equal("Saldo negativo máximo de R$ -20.000,00 ultrapassado.", motivo);
+        }
+
+        [Fact]
+        public void Permite_Lancamento()
+        {
+            var lancamento = new Lancamento
+            {
+                Data = DateTime.Today,
+                TipoDeLancamento = TipoLancamento.Recebimento,
+                Descricao = "Recebimento válido",
+                Conta = "12345-6",
+                Banco = "Banco Z",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "987.654.321-00",
+                Valor = 500m
+            };
+
+            var podeLancar = _service.ValidarLancamento(lancamento, out string motivo);
+
+            Assert.True(podeLancar);
+            Assert.Equal(string.Empty, motivo);
+        }
+    }
+}
---
+++ LancamentosControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using FluxoCaixa.Controllers;
+using FluxoCaixa.DTOs;
+using FluxoCaixa.Repositories;
+using FluxoCaixa.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace FluxoCaixa.Tests
+{
+    public class LancamentosControllerTests
+    {
+        private readonly LancamentosController _controller;
+
+        public LancamentosControllerTests()
+        {
+            var repository = new LancamentoRepository();
+            var service = new FluxoCaixaService(repository);
+            _controller = new LancamentosController(repository, service);
+        }
+
+        [Fact]
+        public void Post_Lancamento_Valido_Deve_Retornar_Ok()
+        {
+            var lancamentoDto = new LancamentoDto
+            {
+                Data = DateTime.Today,
+                TipoDeLancamento = 2,
+                Descricao = "Recebimento Teste",
+                Conta = "12345-6",
+                Banco = "Banco Teste",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "98765432100",
+                Valor = 100m
+            };
+
+            var resultado = _controller.Post(lancamentoDto);
+
+            Assert.IsType<OkObjectResult>(resultado);
+        }
+
+        [Fact]
+        public void Post_Lancamento_Com_Data_Passada_Deve_Retornar_BadRequest()
+        {
+            var lancamentoDto = new LancamentoDto
+            {
+                Data = DateTime.Today.AddDays(-1), // Data no passado
+                TipoDeLancamento = 1, // Pagamento
+                Descricao = "Pagamento inválido",
+                Conta = "12345-6",
+                Banco = "Banco Teste",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "98765432100",
+                Valor = 100m
+            };
+
+            var resultado = _controller.Post(lancamentoDto);
+
+            Assert.IsType<BadRequestObjectResult>(resultado);
+        }
+
+        [Fact]
+        public void Post_TipoDeLancamento_Invalido_BadRequest()
+        {
+            var lancamentoDto = new LancamentoDto
+            {
+                Data = DateTime.Today,
+                TipoDeLancamento = 3, // Tipo inválido (não 1 nem 2)
+                Descricao = "Tipo inválido",
+                Conta = "12345-6",
+                Banco = "Banco Teste",
+                TipoDeConta = "Corrente",
+                CpfCnpj = "98765432100",
+                Valor = 100m
+            };
+
+            var resultado = _controller.Post(lancamentoDto);
+
+            Assert.IsType<BadRequestObjectResult>(resultado);
+        }
+    }
+}
